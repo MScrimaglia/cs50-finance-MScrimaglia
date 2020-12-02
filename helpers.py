f3ce86d897baad4b2db4cb68b728e75e470b93db
MScrimaglia <maxiscrimaglia@gmail.com>
2020-12-02 18:17:01 +0000

    removed api confirmation

--- helpers.py
+++ helpers.py
@@ -40,7 +40,7 @@ def lookup(symbol):
     # Contact API
     try:
         api_key = os.environ.get("API_KEY")
-        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token=pk_6a366ec991904c9c9df8ffd209b1b114")
         response.raise_for_status()
     except requests.RequestException:
         return None
